568e1ba399183ffa4b52e7c0d4ce85f079ee6b92
Aditya Nur Firmansyah <firmansyahadityanur02@gmail.com>
2025-12-01 15:47:25 +0700

    baru

---
+++ UAS Game/Assets/All Script/PlayerAnimator.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator anim;
+    private bool isJumping = false;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void UpdateState(float speed, bool isGrounded, bool isDashing, bool isAttacking, float verticalVelocity, bool isRunning) 
+    {
+        anim.SetFloat("Speed", speed);
+        anim.SetBool("IsGrounded", isGrounded);
+        anim.SetBool("IsDashing", isDashing);
+        anim.SetBool("IsAttacking", isAttacking);
+        anim.SetBool("IsRunning", isRunning);
+
+        // Animasi jatuh biasa
+        bool isFalling = !isGrounded && verticalVelocity < -0.1f;
+        anim.SetBool("IsFalling", isFalling);
+
+        // Reset trigger lompat ketika mendarat
+        if (isGrounded && isJumping)
+        {
+            isJumping = false;
+            anim.ResetTrigger("Jump");
+        }
+    }
+
+    public void TriggerJump()
+    {
+        isJumping = true;
+        anim.ResetTrigger("Jump");
+        anim.SetTrigger("Jump");
+        anim.SetBool("IsGrounded", false);
+    }
+
+    public void TriggerRun()
+    {
+        anim.SetTrigger("Run");
+    }
+
+    public void TriggerDash()
+    {
+        anim.SetTrigger("Dash");
+    }
+
+    public void TriggerAttack()
+    {
+        anim.SetTrigger("Attack");
+    }
+
+    public void TriggerFallDeath()
+    {
+        anim.SetTrigger("FallDeath"); // tambahkan animasi ini di Animator
+    }
+}
